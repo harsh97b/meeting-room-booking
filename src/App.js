a3d8e0b7cc5adac813e6ed124e4b4ed1424b6f27
Harsh Bidhuri <42229671+harsh97b@users.noreply.github.com>
2022-05-09 23:44:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+import '@progress/kendo-theme-default/dist/all.css';  
+import BookingTimeSlot from './components/BookingTimeSlot'
 
 function App() {
+
+  const [meetingRoom, setMeetingRoom] = useState("training");
+  const [name, setName] = useState();
+  const [desc, setDesc] = useState();
+
+  // console.log("here")
+
+  const meetingRoomHandler = (event) =>{
+    var room  = event.target.value
+    console.log("room: ",room)
+    setMeetingRoom(room)
+    // console.log(meetingRoom," ", name," ", desc)
+  }
+  const nameHandler = (event) =>{
+    var n  = event.target.value
+    setName(n)
+    // console.log(meetingRoom," ", name," ", desc)
+    
+  }
+  const descHandler = (event) =>{
+    var d  = event.target.value
+    setDesc(d)
+    // console.log(meetingRoom," ", name," ", desc)
+  }
+  const handleSubmit = (event) =>{
+    event.preventDefault()
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="inputs">
+          <h1>Meeting Room Booking</h1>
+          <select className="custom-field-select" onChange={meetingRoomHandler}>
+            <option selected value="training">Training Room</option>
+            <option value="scrum">Scrum Room</option>
+            <option value="casual">Casual Room</option>
+            <option value="client">Client Room</option>
+          </select>
+          <label className="custom-field one">
+            <input type="text" placeholder=" " value={name} onChange={nameHandler}/>
+            <span className="placeholder">Your Name</span>
+          </label>
+          <label className="custom-field one">
+            <input type="text" placeholder=" " onChange={descHandler}/>
+            <span className="placeholder" >Meeting Description</span>
+          </label>
+        <BookingTimeSlot/>
+        <button className="btn">Book Appointment</button>
+        </div>
+      
     </div>
   );
 }
